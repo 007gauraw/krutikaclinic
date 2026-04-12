8aa3b37bddab611423e2bf32ff0ce1a6c86b01a9
= <=>
2026-04-12 19:39:20 +0530

    updated tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+     terraform {
+       backend "remote" {
+      
+         organization = "krutikaclinic"
+
+        
+         workspaces {
+           name = "example-workspace"
+         }
+       }
+     }
+
+     resource "null_resource" "example" {
+       triggers = {
+         value = "A example resource that does nothing!"
+       }
+     }
