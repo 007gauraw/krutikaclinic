ec168a48cbc3a8575e8e2804ba2962df9e9c507c
= <=>
2026-04-12 19:55:11 +0530

    updated code

--- main.tf
+++ main.tf
@@ -1,17 +1,15 @@
-     terraform {
-       backend "remote" {
-      
-         organization = "krutikaclinic"
+terraform {
+  backend "remote" {
+    organization = "krutikaclinic"
 
-        
-         workspaces {
-           name = "example-workspace"
-         }
-       }
-     }
+    workspaces {
+      name = "example-workspace"
+    }
+  }
+}
 
-     resource "null_resource" "example" {
-       triggers = {
-         value = "A example resource that does nothing!"
-       }
-     }
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
